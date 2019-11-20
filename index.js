bcc4b333e0323423dd9bc57a907e306856cc39cb
Henry Netzer <hnetzer19@gmail.com>
2019-11-20 15:08:06 -0800

    initial api commit based off plaids quickstart

---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const util = require('util');
+const dotenv = require('dotenv')
+const envvar = require('envvar');
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const plaid = require('plaid');
+
+dotenv.config()
+
+const APP_PORT = envvar.number('APP_PORT', 8000);
+const PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
+const PLAID_SECRET = envvar.string('PLAID_SECRET');
+const PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY');
+const PLAID_ENV = envvar.string('PLAID_ENV', 'sandbox');
+
+// PLAID_PRODUCTS is a comma-separated list of products to use when initializing
+// Link. Note that this list must contain 'assets' in order for the app to be
+// able to create and retrieve asset reports.
+const PLAID_PRODUCTS = envvar.string('PLAID_PRODUCTS', 'transactions');
+
+// PLAID_PRODUCTS is a comma-separated list of countries for which users
+// will be able to select institutions from.
+const PLAID_COUNTRY_CODES = envvar.string('PLAID_COUNTRY_CODES', 'US,CA,GB,FR,ES');
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+const ACCESS_TOKEN = null;
+const PUBLIC_TOKEN = null;
+const ITEM_ID = null;
+
+// Initialize the Plaid client
+// Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
+const client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments[PLAID_ENV],
+  {version: '2019-05-29', clientApp: 'Plaid Quickstart'}
+);
+
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+
+// Exchange token flow - exchange a Link public_token for
+// an API access_token
+// https://plaid.com/docs/#exchange-token-flow
+app.post('/get_access_token', function(request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    prettyPrintResponse(tokenResponse);
+    response.json({
+      access_token: ACCESS_TOKEN,
+      item_id: ITEM_ID,
+      error: null,
+    });
+  });
+});
+
+// Retrieve Transactions for an Item
+// https://plaid.com/docs/#transactions
+app.get('/transactions', function(request, response, next) {
+  // Pull transactions for the Item for the last 30 days
+  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error
+      });
+    } else {
+      prettyPrintResponse(transactionsResponse);
+      response.json({error: null, transactions: transactionsResponse});
+    }
+  });
+});
+
+
+const server = app.listen(APP_PORT, function() {
+  console.log('plaid-quickstart server listening on port ' + APP_PORT);
+});
+
+const prettyPrintResponse = response => {
+  console.log(util.inspect(response, {colors: true, depth: 4}));
+};
